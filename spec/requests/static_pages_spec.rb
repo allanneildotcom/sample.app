0a19bc98e7aa3e78bd3ac73035606e7a3229de3b
allanneildotcom <allan@allanneil.com>
2013-03-29 22:38:45 -0400

    dyna

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	describe "home page" do
-		it "should have the content 'Sample App'" do
+	describe "Home page" do
+		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
 		end
-		it "should have the right title" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
 		end
 	end
 	describe "Help page" do
